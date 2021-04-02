9861b68827f365d88c1e1b68288068c967432f25
RenanBuzzi <renangasparbuzzi@gmail.com>
2021-04-02 13:39:38 +0100

    Adding Login UI

--- react-app-donation/src/App.js
+++ react-app-donation/src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
+import "./App.css";
 import { store } from "./actions/store";
 import { Provider } from "react-redux";
-import DataGridCandidates from './components/DataGridCandidates';
+import DataGridCandidates from "./components/DataGridCandidates";
 import { Container } from "@material-ui/core";
 import { ToastProvider } from "react-toast-notifications";
-
+import Login from "./components/Login";
 
 function App() {
   return (
-   <Provider store={store}>
-       <ToastProvider autoDismiss={true}>
-          <Container maxWidth="lg">
-            <DataGridCandidates />
+    <Provider store={store}>
+      <ToastProvider autoDismiss={true}>
+        <Container maxWidth="lg">
+          <Login />
+          {/* <DataGridCandidates /> */}
         </Container>
       </ToastProvider>
-   </Provider>
+    </Provider>
   );
 }
 
